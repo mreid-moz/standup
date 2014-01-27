7b77f3bbf43cace5f1ea8d6d2a31291af7426957
Mark Reid <mreid@mozilla.com>
2014-01-27 09:32:00 -0400

    Add some week utils, pass through 'per_page' count.

--- standup/apps/status/helpers.py
+++ standup/apps/status/helpers.py
@@ -4,31 +4,37 @@ from datetime import date, datetime, timedelta
 from standup.database.helpers import paginate as _paginate
 
 
-def paginate(statuses, page=1, startdate=None, enddate=None):
+def paginate(statuses, page=1, startdate=None, enddate=None, per_page=20):
     from standup.apps.status.models import Status
     if startdate:
         statuses = statuses.filter(Status.created >= startdate)
     if enddate:
         statuses = statuses.filter(Status.created <= enddate)
-    return _paginate(statuses, int(page))
+    return _paginate(statuses, int(page), per_page=per_page)
 
 
 def startdate(request):
     dates = request.args.get('dates')
     day = request.args.get('day')
+    week = request.args.get('week')
     if dates == '7d':
         return date.today() - timedelta(days=7)
     elif dates == 'today':
         return date.today()
     elif isday(day):
         return get_day(day)
+    elif isday(week):
+        return get_day(week)
     return None
 
 
 def enddate(request):
     day = request.args.get('day')
+    week = request.args.get('week')
     if isday(day):
         return get_day(day) + timedelta(days=1)
+    elif isday(week):
+        return get_day(week) + timedelta(days=7)
     return None
 
 
